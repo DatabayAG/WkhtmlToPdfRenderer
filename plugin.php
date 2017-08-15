ed6aa4893153faac9980d267c2ffa0a735f777dc
Guido Vollbach <gvollbach@databay.de>
2017-08-15 12:34:39 +0200

    first commit

---
+++ plugin.php
@@ -0,0 +1,8 @@
+<?php
+/* Copyright (c) 1998-2016 ILIAS open source, Extended GPL, see docs/LICENSE */
+$id                = 'wkhtp';
+$version           = '0.0.2';
+$ilias_min_version = '5.2.0';
+$ilias_max_version = '5.3.999';
+$responsible       = 'Databay AG';
+$responsible_mail  = 'gvollbach@databay.de';
---
+++ sql/dbupdate.php
@@ -0,0 +1,45 @@
+<#1>
+<?php
+/*
+ * Use to register a renderer:
+ * ===========================
+ *
+ * require_once './Services/PDFGeneration/classes/class.ilPDFCompInstaller.php';
+ *
+ * $renderer = 'DummyRenderer';
+ * $path =  'Customizing/global/plugins/Services/PDFGeneration/Renderer/DummyRenderer/classes/class.ilDummyRendererPlugin.php';
+ * ilPDFCompInstaller::registerRenderer($renderer, $path);
+ *
+ * $purpose = 'UserResults'; // According to name given. Call multiple times.
+ * ilPDFCompInstaller::registerRendererAvailability($renderer, $service, $purpose);
+ */
+/*
+ * Use to register a service/purpose:
+ * ==================================
+ *
+ * require_once './Services/PDFGeneration/classes/class.ilPDFCompInstaller.php';
+ *
+ * ilPDFCompInstaller::registerPurpose($service,$purpose,$preferred);
+ * or uninstall one:
+ * ilPDFCompInstaller::unregisterPurpose($service, $purpose);
+ * or uninstall all:
+ * ilPDFCompInstaller::flushPurposes($service);
+ *
+ */
+?>
+<#2>
+<?php
+require_once './Services/PDFGeneration/classes/class.ilPDFCompInstaller.php';
+
+ $renderer = 'WkhtmlToPdf';
+ $path =  'Customizing/global/plugins/Services/PDFGeneration/Renderer/WkhtmlToPdfRenderer/classes/class.ilWkhtmlToPdfRendererPlugin.php';
+ ilPDFCompInstaller::registerRenderer($renderer, $path);
+
+ $service = 'Test';
+ $purpose = 'UserResults'; // According to name given. Call multiple times.
+ ilPDFCompInstaller::registerRendererAvailability($renderer, $service, $purpose);
+ 
+ $purpose = 'PrintViewOfQuestion'; // According to name given. Call multiple times.
+ ilPDFCompInstaller::registerRendererAvailability($renderer, $service, $purpose);
+
+?>
